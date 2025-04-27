2ac4a3770a78724cfed0310cd7fe00585f109ae2
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-04-27 20:41:22 -0300

    implements delete user

--- src/model/repository/user_repository.go
+++ src/model/repository/user_repository.go
@@ -27,15 +27,19 @@ type UserRepository interface {
 		userDomain model.UserDomainInterface,
 	) (model.UserDomainInterface, *rest_err.RestErr)
 
+	UpdateUser(
+		userId string,
+		userDomain model.UserDomainInterface,
+	) *rest_err.RestErr
+
+	DeleteUser(
+		userId string,
+	) *rest_err.RestErr
+
 	FindUserByEmail(
 		email string,
 	) (model.UserDomainInterface, *rest_err.RestErr)
 	FindUserByID(
 		id string,
 	) (model.UserDomainInterface, *rest_err.RestErr)
-
-	UpdateUser(
-		userId string,
-		userDomain model.UserDomainInterface,
-	) *rest_err.RestErr
 }
