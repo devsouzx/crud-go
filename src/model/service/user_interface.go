126a44ca700066d4e2fc6a03ed590b65f7e94ef4
devsouzx <joaoemanuelmarinhosousa@gmail.com>
2025-04-27 20:32:14 -0300

    implements update user

--- src/model/service/user_interface.go
+++ src/model/service/user_interface.go
@@ -17,9 +17,16 @@ type userDomainService struct {
 }
 
 type UserDomainService interface {
-	CreateUserServices(model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestErr)
-	UpdateUserServices(string, model.UserDomainInterface) *rest_err.RestErr
-	FindUserByIDServices(string) (model.UserDomainInterface, *rest_err.RestErr)
-	FindUserByEmailServices(string) (model.UserDomainInterface, *rest_err.RestErr)
-	DeleteUserServices(string) *rest_err.RestErr
+	CreateUserServices(model.UserDomainInterface) (
+		model.UserDomainInterface, *rest_err.RestErr)
+
+	FindUserByIDServices(
+		id string,
+	) (model.UserDomainInterface, *rest_err.RestErr)
+	FindUserByEmailServices(
+		email string,
+	) (model.UserDomainInterface, *rest_err.RestErr)
+
+	UpdateUser(string, model.UserDomainInterface) *rest_err.RestErr
+	DeleteUser(string) *rest_err.RestErr
 }
